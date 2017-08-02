3bdc00be151200e7a4bb304d95428790b64f5374
Olivier <olivier_boniteau@yahoo.fr>
2017-08-02 20:46:56 +0200

    help update. package update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, Http } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 // translate
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 // AoT requires an exported function for factories
-export function HttpLoaderFactory(http: Http) {
-  // add './' for targeting local app directory
-  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http);
 }
 
 
@@ -44,19 +44,20 @@ import {  OrderbyPipe } from './shared/filters';
 
 @NgModule({
   imports: [
-    BrowserModule,
     FormsModule,
     HttpModule,
     routing,
     MaterialModule,
     NoopAnimationsModule,
+    BrowserModule,
+    HttpClientModule,
     TranslateModule.forRoot({
-      loader: {
-        provide: TranslateLoader,
-        useFactory: HttpLoaderFactory,
-        deps: [Http]
-      }
-    })
+                loader: {
+                    provide: TranslateLoader,
+                    useFactory: HttpLoaderFactory,
+                    deps: [HttpClient]
+                }
+            })
   ],
   declarations: [
 
