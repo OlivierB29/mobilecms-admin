6069669bf3b04695e37ecb66dcec6057c812b96c
Olivier <olivier_boniteau@yahoo.fr>
2020-09-10 18:49:35 +0200

    alternate prod version

---
+++ src/environments/environment.prod2.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  log: 'info',
+  server: '',
+  website: '/#',
+  defaultlocale: 'fr',
+  usemockbackend: false,
+  api: '/mobilecmsapi/v2/cmsapi',
+  adminapi: '/mobilecmsapi/v2/adminapi',
+  fileapi: '/mobilecmsapi/v2/fileapi',
+  authapi: '/mobilecmsapi/v2/authapi'
+};
